7a26380bc419c3a497e481dce5aa7856b1a5e304
JadenKim-dev <jaden.kim5179@gmail.com>
2022-07-17 22:05:23 +0000

    feat(error): custom error 정의

---
+++ server/middlewares/error.js
@@ -0,0 +1,15 @@
+export const asyncWrapper = (asyncFunc) => async (req, res, next) => {
+  try {
+    await asyncFunc(req, res, next);
+  } catch (err) {
+    require("#utils/logger/index").logger.error({
+      err,
+      url: req.url,
+      query: req.query,
+      params: req.params,
+      body: req.body,
+      headers: req.headers,
+    });
+    next(err);
+  }
+};
