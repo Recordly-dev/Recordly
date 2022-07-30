c0c12ab1771d3cef35b4468b1d9026ac416de408
yhs4157 <yhs4157@naver.com>
2022-07-30 19:22:25 +0900

    feat: image uploader 구현

--- client/src/components/Editor/Editor.jsx
+++ client/src/components/Editor/Editor.jsx
@@ -1,14 +1,15 @@
-import React, { useCallback, useMemo } from "react";
-import { createEditor } from "slate";
-import { withHistory } from "slate-history";
-import { Toolbar } from "../Components/Components";
-import { MarkButton, BlockButton, Element, Leaf } from "../RichText/RichText";
-import { Editable, withReact, Slate } from "slate-react";
+import React, {useCallback, useMemo} from 'react';
+import {createEditor} from 'slate';
+import { withHistory } from 'slate-history';
+import {Toolbar} from '../Components/Components'; 
+import {MarkButton, BlockButton, Leaf} from '../RichText/RichText'; 
+import { Editable, withReact, Slate } from 'slate-react';
+import {Image, withImages, InsertImageButton} from '../ImageUploader/ImageUploader';
 
 const Editor = () => {
-  const renderElement = useCallback((props) => <Element {...props} />, []);
-  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const renderElement = useCallback(props => <Element {...props} />, [])
+  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+  const editor = useMemo(() => withImages(withHistory(withReact(createEditor())), []))
 
   return (
     <Slate editor={editor} value={initialValue}>
@@ -26,6 +27,7 @@ const Editor = () => {
         <BlockButton format="center" icon="format_align_center" />
         <BlockButton format="right" icon="format_align_right" />
         <BlockButton format="justify" icon="format_align_justify" />
+        <InsertImageButton />
       </Toolbar>
       <Editable
         renderElement={renderElement}
@@ -45,43 +47,97 @@ const Editor = () => {
         }}*/
       />
     </Slate>
-  );
-};
+  )
+}
+
+const Element = ({ attributes, children, element }) => {
+  const style = { textAlign: element.align }
+  const props = {attributes, children, element};
+  switch (element.type) {
+    case 'block-quote':
+      return (
+        <blockquote style={style} {...attributes}>
+          {children}
+        </blockquote>
+      )
+    case 'bulleted-list':
+      return (
+        <ul style={style} {...attributes}>
+          {children}
+        </ul>
+      )
+    case 'heading-one':
+      return (
+        <h1 style={style} {...attributes}>
+          {children}
+        </h1>
+      )
+    case 'heading-two':
+      return (
+        <h2 style={style} {...attributes}>
+          {children}
+        </h2>
+      )
+    case 'list-item':
+      return (
+        <li style={style} {...attributes}>
+          {children}
+        </li>
+      )
+    case 'numbered-list':
+      return (
+        <ol style={style} {...attributes}>
+          {children}
+        </ol>
+      )
+    case 'image':
+      return <Image {...props} />
+    default:
+      return (
+        <p style={style} {...attributes}>
+          {children}
+        </p>
+      )
+  }
+}
+
 
 const initialValue = [
   {
-    type: "paragraph",
+    type: 'paragraph',
     children: [
-      { text: "This is editable " },
-      { text: "rich", bold: true },
-      { text: " text, " },
-      { text: "much", italic: true },
-      { text: " better than a " },
-      { text: "<textarea>", code: true },
-      { text: "!" },
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
     ],
   },
   {
-    type: "paragraph",
+    type: 'paragraph',
     children: [
       {
-        text: "Since it's rich text, you can do things like turn a selection of text ",
+        text:
+          "Since it's rich text, you can do things like turn a selection of text ",
       },
-      { text: "bold", bold: true },
+      { text: 'bold', bold: true },
       {
-        text: ", or add a semantically rendered block quote in the middle of the page, like this:",
+        text:
+          ', or add a semantically rendered block quote in the middle of the page, like this:',
       },
     ],
   },
   {
-    type: "block-quote",
-    children: [{ text: "A wise quote." }],
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
   },
   {
-    type: "paragraph",
-    align: "center",
-    children: [{ text: "Try it out for yourself!" }],
+    type: 'paragraph',
+    align: 'center',
+    children: [{ text: 'Try it out for yourself!' }],
   },
-];
+]
 
 export default Editor;
