ebd5dc956be594ee5affec4be9e72bb282ea6e9f
pmhxhsj <minho7952@naver.com>
2022-08-02 23:34:05 +0900

    commit test

--- client/src/containers/GoogleOauth/GoogleOauth.jsx
+++ client/src/containers/GoogleOauth/GoogleOauth.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
 import axios from "axios";
 
 const GoogleOauth = () => {
-  const navigate = useNavigate();
   const onClickGoogleLogin = () => {
-    // axios.get("http://localhost:8080/api/auth/google");
+    axios.get("http://localhost:8080/api/auth/google/callback");
   };
   return (
     <div>
