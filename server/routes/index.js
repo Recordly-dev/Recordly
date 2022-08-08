e30fd2471f1a39ba575516c1df964ffcd3020920
JadenKim-dev <jaden.kim5179@gmail.com>
2022-08-08 20:25:39 +0900

    feat(workspace): workspace 라우터 작성

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 
-// import userRouter from "./user.js";
+import workspaceRouter from "./workspace.js";
 
 const router = express.Router();
 
-// router.use("/user", userRouter);
+router.use("/workspace", workspaceRouter);
 
 export default router;
---
+++ server/routes/workspace.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import modWorkspace from "#models/workspace.js";
+
+const router = express.Router();
+
+router.route("/").post(async (req, res, next) => {
+  try {
+    const { title, workspaceType } = req.body;
+    const { id: writerId } = req.session.user;
+    const workspace = await modWorkspace.create({
+      title,
+      workspaceType,
+      createdAt: Date.now(),
+      editedAt: Date.now(),
+      writer: writerId,
+    });
+    res.status(201);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+export default router;
