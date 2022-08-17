b68ef5d98aa2f9035a0796d5265bca6f4585349d
pmhxhsj <minho7952@naver.com>
2022-08-17 19:37:51 +0900

    feat: slate

--- client/src/components/Editor/Editor.jsx
+++ client/src/components/Editor/Editor.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useMemo } from "react";
 import { createEditor } from "slate";
 import { withHistory } from "slate-history";
-import { Toolbar } from "../Components/Components";
+import { Toolbar, Icon } from "../Components/Components";
 import { MarkButton, BlockButton, Leaf } from "../RichText/RichText";
 import { Editable, withReact, Slate } from "slate-react";
 import {
@@ -9,6 +9,8 @@ import {
   withImages,
   InsertImageButton,
 } from "../ImageUploader/ImageUploader";
+
+import { css, cx } from "@emotion/css";
 
 const Editor = () => {
   const renderElement = useCallback((props) => <Element {...props} />, []);
@@ -18,41 +20,46 @@ const Editor = () => {
   );
 
   return (
-    <Slate editor={editor} value={initialValue}>
-      <Toolbar>
-        <MarkButton format="bold" icon="format_bold" />
-        <MarkButton format="italic" icon="format_italic" />
-        <MarkButton format="underline" icon="format_underlined" />
-        <MarkButton format="code" icon="code" />
-        <BlockButton format="heading-one" icon="looks_one" />
-        <BlockButton format="heading-two" icon="looks_two" />
-        <BlockButton format="block-quote" icon="format_quote" />
-        <BlockButton format="numbered-list" icon="format_list_numbered" />
-        <BlockButton format="bulleted-list" icon="format_list_bulleted" />
-        <BlockButton format="left" icon="format_align_left" />
-        <BlockButton format="center" icon="format_align_center" />
-        <BlockButton format="right" icon="format_align_right" />
-        <BlockButton format="justify" icon="format_align_justify" />
-        <InsertImageButton />
-      </Toolbar>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        placeholder="Enter some rich text…"
-        spellCheck
-        autoFocus
-        /*
-        onKeyDown={event => {
-          for (const hotkey in HOTKEYS) {
-            if (isHotkey(hotkey, event)) {
-              event.preventDefault()
-              const mark = HOTKEYS[hotkey]
-              toggleMark(editor, mark)
-            }
-          }
-        }}*/
-      />
-    </Slate>
+    <AppWrap>
+      <Slate editor={editor} value={initialValue}>
+        <div className="banner">
+          <Savebar>
+            <Input placeholder="Title Input" />
+            <Button>
+              <Icon>save</Icon>
+            </Button>
+          </Savebar>
+          <Toolbar>
+            <MarkButton format="bold" icon="format_bold" />
+            <MarkButton format="italic" icon="format_italic" />
+            <MarkButton format="underline" icon="format_underlined" />
+            <MarkButton format="code" icon="code" />
+            <BlockButton format="heading-one" icon="looks_one" />
+            <BlockButton format="heading-two" icon="looks_two" />
+            <BlockButton format="block-quote" icon="format_quote" />
+            <BlockButton format="numbered-list" icon="format_list_numbered" />
+            <BlockButton format="bulleted-list" icon="format_list_bulleted" />
+            <BlockButton format="left" icon="format_align_left" />
+            <BlockButton format="center" icon="format_align_center" />
+            <BlockButton format="right" icon="format_align_right" />
+            <BlockButton format="justify" icon="format_align_justify" />
+            <InsertImageButton />
+          </Toolbar>
+        </div>
+
+        <DocumentWrap>
+          <DocumentMain>
+            <Editable
+              renderElement={renderElement}
+              renderLeaf={renderLeaf}
+              placeholder="Enter some rich text…"
+              spellCheck
+              autoFocus
+            />
+          </DocumentMain>
+        </DocumentWrap>
+      </Slate>
+    </AppWrap>
   );
 };
 
@@ -106,6 +113,97 @@ const Element = ({ attributes, children, element }) => {
       );
   }
 };
+
+const DocumentWrap = React.forwardRef(({ className, ...props }, ref) => (
+  <div {...props} ref={ref} className={cx(className, css``)}></div>
+));
+
+const DocumentMain = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    {...props}
+    ref={ref}
+    className={cx(
+      className,
+      css`
+        height: 1056px;
+        width: 816px;
+        padding: 15px;
+        background-color: white;
+      `
+    )}
+  ></div>
+));
+
+const AppWrap = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    {...props}
+    ref={ref}
+    className={cx(
+      className,
+      css`
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+      `
+    )}
+  ></div>
+));
+
+const Savebar = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    {...props}
+    ref={ref}
+    className={cx(
+      className,
+      css`
+        position: relative;
+        width: 816px;
+        padding: 1px 18px 7px;
+        margin: 0 -20px;
+        border-bottom: 2px solid #eee;
+        margin-bottom: 20px;
+      `
+    )}
+  ></div>
+));
+
+const Input = React.forwardRef(({ className, ...props }, ref) => (
+  <input
+    {...props}
+    ref={ref}
+    className={cx(
+      className,
+      css`
+        border: 1px solid transparent;
+        border-radius: 2px !important;
+        font-size: 18px;
+        font-variant-ligatures: no-contextual;
+        height: 20px;
+        line-height: 22px;
+        margin: 0;
+        min-width: 1px;
+        padding: 2px 7px;
+        &:hover {
+          outline: solid;
+        }
+      `
+    )}
+  ></input>
+));
+
+const Button = React.forwardRef(({ className, ...props }, ref) => (
+  <button
+    {...props}
+    ref={ref}
+    className={cx(
+      className,
+      css`
+        border: 0;
+        background: none;
+      `
+    )}
+  ></button>
+));
 
 const initialValue = [
   {
