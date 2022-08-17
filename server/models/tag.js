ffd0acb6fd5a756a7c86e3aa454547d957e30062
JadenKim-dev <jaden.kim5179@gmail.com>
2022-08-17 20:49:09 +0900

    slate localStorage save

---
+++ server/models/tag.js
@@ -0,0 +1,32 @@
+import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+
+const workspaceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  workspaceType: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  editedAt: {
+    type: Date,
+    required: true,
+  },
+  writer: {
+    type: ObjectId,
+    required: true,
+    ref: "User",
+  },
+  // folder: {
+  //   type: ObjectId,
+  //   ref: "",
+  // },
+});
+
+export default mongoose.model("Workspace", workspaceSchema);
