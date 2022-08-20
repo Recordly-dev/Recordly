b6e161db09471ca7b2dfaed434ff2714d0aa6ff8
pmhxhsj <minho7952@naver.com>
2022-08-20 16:18:47 +0900

    feat: slate 연동

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "pages/loginPage";
 import Main from "pages/main";
 import Editor from "components/Editor";
-import EditorPage from "pages/editorPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/main" element={<Main />} />
-      <Route path="/editor" element={<EditorPage />} />
-      <Route path="/editor2" element={<Editor />} />
+      <Route path="/workspace/:tid" element={<Editor />} />
     </Routes>
   );
 };
