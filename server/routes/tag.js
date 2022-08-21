50c310642a97c96f863017c7902dd005ff45644c
JadenKim-dev <jaden.kim5179@gmail.com>
2022-08-21 20:19:59 +0900

    tag logic segregated

--- server/routes/tag.js
+++ server/routes/tag.js
@@ -1,57 +1,11 @@
 import express from "express";
-import moment from "moment-timezone";
 
-import modTag from "#models/tag.js";
-import modWorkspace from "#models/workspace.js";
+import tagApi from "#controllers/tagApi.js";
 
 const router = express.Router();
 
-router.route("/").get(async (req, res, next) => {
-  try {
-    const { id: writerId } = req.user;
-    const tags = await modTag
-      .find({ writer: writerId })
-      .populate("workspaces", "title");
+router.route("/").get(tagApi.getTagsOfCurrentUser);
 
-    res.json({ data: tags });
-  } catch (err) {
-    console.error(err);
-    console.dir(err);
-    next(err);
-  }
-});
-
-router.route("/").post(async (req, res, next) => {
-  try {
-    const { name, workspaceId } = req.body;
-    const { id: writerId } = req.user;
-
-    const findTag = await modTag.findOne({ name, writer: writerId }).exec();
-    if (findTag) {
-      findTag.workspaces.push(workspaceId);
-      await modWorkspace.update(
-        { _id: workspaceId },
-        { $push: { tags: findTag._id } }
-      );
-      return res.status(201).json({ data: findTag });
-    } else {
-      const newTag = await modTag.create({
-        name,
-        createdAt: moment().add(9, "hour").format("YYYY-MM-DD HH:mm:ss"),
-        writer: writerId,
-        workspaces: [workspaceId],
-      });
-      await modWorkspace.update(
-        { _id: workspaceId },
-        { $push: { tags: newTag._id } }
-      );
-      return res.status(201).json({ data: newTag });
-    }
-  } catch (err) {
-    console.error(err);
-    console.dir(err);
-    next(err);
-  }
-});
+router.route("/").post(tagApi.createTag);
 
 export default router;
