8cbec1b956904b9d6eba4eba01e73108aaf5f258
JadenKim-dev <jaden.kim5179@gmail.com>
2022-08-28 11:49:58 +0900

    docker development environment complete

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,15 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// src/setupProxy.js
 module.exports = function (app) {
-  app.use(
-    "/api",
-    (req, res, next) => {
-      next();
-    },
-    createProxyMiddleware({
-      target: "http://localhost:8080", // 비즈니스 서버 URL 설정
-      changeOrigin: true,
-    })
-  );
+  if (!process.env.IS_DOCKER) {
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      })
+    );
+  }
 };
