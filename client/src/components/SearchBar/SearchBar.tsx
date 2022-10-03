d6ba3ddbdcbe5e2bced78694d85bae3dcfacca30
pmhxhsj <minho7952@naver.com>
2022-10-03 17:17:35 +0900

    feat: 검색 기능 구현
    - 엔터 누르면 서버통신되고, 글자수 0이면 자동으로 서버통신해서 데이터 가져오게 구현

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,36 @@
+import React, { useState, useEffect } from "react";
 import { Input, InputGroup, InputGroupText } from "reactstrap";
+
+import { filterWorkspaceList } from "store/slice/workspcaeSlice";
+import { useDispatch } from "store";
 
 import searchIcon from "./assets/images/search-icon.png";
 import styles from "./SearchBar.module.scss";
 
 const SearchBar = () => {
+  const dispatch = useDispatch();
+
+  const [searchValue, setSearchValue] = useState("");
+  const [isSearched, setIsSearched] = useState(false);
+
+  const handleChangeSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+
+    if (e.target.value === "") {
+      setIsSearched(false);
+    }
+  };
+
+  const handleOnKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setIsSearched(true);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(filterWorkspaceList(searchValue));
+  }, [isSearched]);
+
   return (
     <div className={styles.SearchBar}>
       <InputGroup>
@@ -16,6 +43,9 @@ const SearchBar = () => {
         </InputGroupText>
         <Input
           id={styles.searchInput}
+          value={searchValue}
+          onChange={handleChangeSearchValue}
+          onKeyDown={handleOnKeyPress}
           className={styles.SearchBar__input}
           placeholder="검색"
         />
