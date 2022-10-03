855bca5750623f1027d15ca4b628300d1110c733
pmhxhsj <minho7952@naver.com>
2022-10-04 01:28:39 +0900

    refactor: try-catch문으로 에러처리

--- client/src/store/slice/workspcaeSlice.tsx
+++ client/src/store/slice/workspcaeSlice.tsx
@@ -21,49 +21,61 @@ export const { setWorkspace } = workspaceSlice.actions;
 
 export const fetchWorkspace = () => {
   return async (dispatch: Function) => {
-    axios
-      .get("api/workspace")
-      .then((res) => {
-        dispatch(setWorkspace(res.data));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      axios
+        .get("api/workspace")
+        .then((res) => {
+          dispatch(setWorkspace(res.data));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
   };
 };
 
 export const filterWorkspaceList = (value: string) => {
   return async (dispatch: Function) => {
-    axios
-      .get("api/workspace")
-      .then((res) => {
-        const filterData = res.data.filter((v: IWorkspace) =>
-          v.title.includes(value)
-        );
-        console.log(filterData);
-        dispatch(setWorkspace(filterData));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      axios
+        .get("api/workspace")
+        .then((res) => {
+          const filterData = res.data.filter((v: IWorkspace) =>
+            v.title.includes(value)
+          );
+          console.log(filterData);
+          dispatch(setWorkspace(filterData));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
   };
 };
 
 export const sortWorkspaceList = (type: string) => {
   return async (dispatch: Function) => {
-    axios
-      .get("api/workspace")
-      .then((res) => {
-        if (type === "newest") {
-          dispatch(setWorkspace(res.data));
-        } else if (type === "oldest") {
-          const sortedData = sortBy(res.data, "editedAt");
-          dispatch(setWorkspace(sortedData));
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      axios
+        .get("api/workspace")
+        .then((res) => {
+          if (type === "newest") {
+            dispatch(setWorkspace(res.data));
+          } else if (type === "oldest") {
+            const sortedData = sortBy(res.data, "editedAt");
+            dispatch(setWorkspace(sortedData));
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
   };
 };
 
