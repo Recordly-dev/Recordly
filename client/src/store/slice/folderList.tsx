9ce21e0595fa087d74138da23abed0508c2e79ea
pmhxhsj <minho7952@naver.com>
2022-10-25 20:51:29 +0900

    refactor: folder silce 리팩토링

--- client/src/store/slice/folderList.tsx
+++ client/src/store/slice/folderList.tsx
@@ -1,8 +1,45 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+export const fetchFolderList = createAsyncThunk("/api/folder", async () => {
+  const response = await axios.get("/api/folder");
+
+  return response?.data;
+});
+
+export const deleteFolderList = createAsyncThunk(
+  "api/folder",
+  async (arg: { uid: String }, { dispatch }) => {
+    await axios.delete(`/api/folder/${arg.uid}`);
+
+    dispatch(fetchFolderList());
+  }
+);
+
+export const postFolderList = createAsyncThunk(
+  "api/folder",
+  async (arg: { title: String }, { dispatch }) => {
+    await axios.post("/api/folder/", {
+      title: arg.title,
+    });
+
+    dispatch(fetchFolderList());
+  }
+);
+
+export const patchFolderList = createAsyncThunk(
+  "api/folder",
+  async (arg: { uid: String; title: String }, { dispatch }) => {
+    await axios.patch(`/api/folder/${arg.uid}`, {
+      folderId: arg.uid,
+      title: arg.title,
+    });
+    dispatch(fetchFolderList());
+  }
+);
 const initialState = {
   folderList: [],
+  isFolderLoading: false,
 };
 
 const tagSlice = createSlice({
@@ -13,25 +50,43 @@ const tagSlice = createSlice({
       state.folderList = action.payload;
     },
   },
+  extraReducers: {
+    [fetchFolderList.pending.type]: (state) => {
+      state.isFolderLoading = true;
+    },
+    [fetchFolderList.fulfilled.type]: (state, action) => {
+      state.folderList = action.payload;
+      state.isFolderLoading = false;
+    },
+    [postFolderList.pending.type]: (state) => {
+      state.isFolderLoading = true;
+    },
+    [postFolderList.fulfilled.type]: (state) => {
+      state.isFolderLoading = false;
+    },
+    [deleteFolderList.pending.type]: (state) => {
+      state.isFolderLoading = true;
+    },
+    [deleteFolderList.fulfilled.type]: (state) => {
+      state.isFolderLoading = false;
+    },
+    [patchFolderList.pending.type]: (state) => {
+      state.isFolderLoading = true;
+    },
+    [patchFolderList.fulfilled.type]: (state) => {
+      state.isFolderLoading = false;
+    },
+  },
 });
 
 export const { setFolderList } = tagSlice.actions;
 
-export const fetchFolderList = () => {
-  return async (dispatch: Function) => {
-    try {
-      axios
-        .get("/api/folder")
-        .then((res) => {
-          dispatch(setFolderList(res?.data));
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+export const actions = {
+  fetchFolderList,
+  postFolderList,
+  deleteFolderList,
+  patchFolderList,
+  setFolderList,
 };
 
 export default tagSlice.reducer;
