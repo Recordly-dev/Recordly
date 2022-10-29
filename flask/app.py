1a7cdeb8d368c6b2de0de4fd3ec89210c35b0b95
JadenKim-dev <jaden.kim5179@gmail.com>
2022-10-29 16:58:21 +0900

    tags json으로 응답 받을 수 있도록 구현

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,23 @@
-application = Flask(__name__)
+from flask import Flask, request, jsonify, json
+import kobert
 
+application = Flask(__name__)
 
 @application.route("/kobert/")
 def hello():
     return "Hello kobert!"
 
-@application.route('/kobert/tags/<text>')
-def select(text):
-    print(kobert.getTags(text))
-    return 'hi %s' % kobert.getTags(text)
+@application.route('/kobert/tags', methods=['POST'])
+def getTags():
+    text = request.json['text']
+    print(text)
+    if not text: return jsonify(tags=[])
 
-#if __name__ == "__main__":
-#    application.run(host='0.0.0.0', port=5000)
+    try: 
+        tags = kobert.getTags(text)
+        return jsonify(tags=tags)
+    except:
+        return jsonify(tags=[])
+
+if __name__ == "__main__":
+   application.run(host='0.0.0.0', port=5000)
