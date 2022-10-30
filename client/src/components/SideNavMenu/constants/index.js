f5bdf78364b58c34d94355ef48d85c8e15234426
pmhxhsj <minho7952@naver.com>
2022-10-30 13:08:57 +0900

    refactor: 사이드바 리팩토링

---
+++ client/src/components/SideNavMenu/constants/index.js
@@ -0,0 +1,16 @@
+const NAV_ITEM = [
+  {
+    title: "Dashboard",
+    link: "/main",
+  },
+  {
+    title: "Favorites",
+    link: "/favorites",
+  },
+  {
+    title: "Tags",
+    link: "/tags",
+  },
+];
+
+export default { NAV_ITEM };
