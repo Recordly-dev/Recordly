048e7d57063efa59e5b2b3a02aef82a12400cb51
pmhxhsj <minho7952@naver.com>
2022-11-05 18:29:54 +0900

    feat: 태그 검색 api 연동

--- client/src/components/Tag/Tag.tsx
+++ client/src/components/Tag/Tag.tsx
@@ -1,52 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { useDispatch } from "store";
-import { useSelector } from "react-redux";
 import { Button } from "reactstrap";
 
 import { actions as workspaceActions } from "store/slice/workspaceSlice";
 
-import { IWorkspace } from "types/workspace";
 import cn from "classnames";
 
 import styles from "./Tag.module.scss";
 
-const Tag = ({ tagList }: { tagList: any }) => {
+const Tag = ({
+  tagList,
+  tagInputValue,
+}: {
+  tagList: any;
+  tagInputValue: string;
+}) => {
+  const [currentSeleteTagId, setCurrentSeleteTagId] = useState("");
   const dispatch = useDispatch();
-  const workspaceList = useSelector(
-    (state: any) => state.workspace.workspaceList
-  );
 
-  const getWorkspaceHaveTags = (workspace: IWorkspace[], tagName: string) => {
-    const filterWorkspace = workspace.filter((v: any) => {
-      const tagsArr = v.tags.map((tag: any) => tag.name);
-
-      return tagsArr.includes(tagName);
-    });
-
-    dispatch(workspaceActions.setWorkspaceList(filterWorkspace));
+  const getWorkspaceHaveTags = (tagId: string) => {
+    if (currentSeleteTagId === tagId) {
+      dispatch(workspaceActions.fetchAllWorkspaceList());
+    } else {
+      setCurrentSeleteTagId(tagId);
+      dispatch(workspaceActions.fetchWorkspacesWithTag({ tagId }));
+    }
   };
 
   return (
     <div className={cn(styles.Tag, "d-flex", "flex-column")}>
-      {tagList.map((tag: any) => (
-        <Button
-          className={cn(
-            styles.Tag__tag,
-            "d-flex",
-            "align-items-center",
-            "justify-content-center"
-          )}
-          color="primary"
-          size="md"
-          onClick={() => getWorkspaceHaveTags(workspaceList, tag.name)}
-        >
-          <span>{tag?.name}</span>
-          <span className={styles.Tag__tag__count}>
-            ({tag.workspaces?.length})
-          </span>
-        </Button>
-      ))}
+      {tagList.map(
+        (tag: any) =>
+          tag.name.includes(tagInputValue) && (
+            <Button
+              className={cn(
+                styles.Tag__tag,
+                "d-flex",
+                "align-items-center",
+                "justify-content-center"
+              )}
+              color="primary"
+              size="md"
+              onClick={() => getWorkspaceHaveTags(tag?._id)}
+            >
+              <span>{tag?.name}</span>
+              <span className={styles.Tag__tag__count}>
+                ({tag.workspaces?.length})
+              </span>
+            </Button>
+          )
+      )}
     </div>
   );
 };
