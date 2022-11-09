69aee80630401a54c0d9a7e7914478f633a14aa1
pmhxhsj <minho7952@naver.com>
2022-11-10 01:17:07 +0900

    feat: 태그 및 태그리스트 개발 및 디자인

--- client/src/components/TagList/TagList.tsx
+++ client/src/components/TagList/TagList.tsx
@@ -1,12 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+
+import { useDispatch } from "store";
+
 import { actions as workspaceActions } from "store/slice/workspaceSlice";
 
-import Tag from "components/Tag";
 import styles from "./TagList.module.scss";
+import Tag from "components/Tag";
 
-const TagList = () => {
-  return <></>;
+const TagList = ({
+  tagList,
+  tagInputValue,
+}: {
+  tagList: any;
+  tagInputValue: string;
+}) => {
+  const [isSeletedTag, setIsSeletedTag] = useState(false);
+  const [currentSeleteTagId, setCurrentSeleteTagId] = useState("");
+  const dispatch = useDispatch();
+
+  const getWorkspaceHaveTags = (tagId: string) => {
+    if (currentSeleteTagId === tagId && isSeletedTag) {
+      setIsSeletedTag(false);
+      dispatch(workspaceActions.fetchAllWorkspaceList());
+    } else {
+      setIsSeletedTag(true);
+      setCurrentSeleteTagId(tagId);
+      dispatch(workspaceActions.fetchWorkspacesWithTag({ tagId }));
+    }
+  };
+
+  return (
+    <div className={styles.TagList}>
+      {tagList.map(
+        (tag: any) =>
+          tag.name.includes(tagInputValue) && (
+            <Tag
+              id={tag?._id}
+              name={tag?.name}
+              count={tag?.workspaces?.length}
+              getWorkspaceHaveTags={getWorkspaceHaveTags}
+            />
+          )
+      )}
+    </div>
+  );
 };
 
 export default TagList;
