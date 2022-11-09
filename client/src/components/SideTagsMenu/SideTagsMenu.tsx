ddc00b6f7164f467c6d606675a245f1293ef0b36
pmhxhsj <minho7952@naver.com>
2022-11-10 01:16:23 +0900

    feat: 사이드 태그 창 기능 개발 및 디자인

--- client/src/components/SideTagsMenu/SideTagsMenu.tsx
+++ client/src/components/SideTagsMenu/SideTagsMenu.tsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { Input } from "reactstrap";
+import cn from "classnames";
 import { useSelector } from "react-redux";
 import { useDispatch } from "store";
 
 import { actions as tagListActions } from "store/slice/tagSlice";
 
-import Tag from "components/Tag";
+import TagList from "components/TagList";
+
+import SortBigOrderIcon from "common/assets/icons/SortBigOrderIcon";
+import SortBasicOrderIcon from "common/assets/icons/SortBasicOrderIcon";
 
 import styles from "./SideTagsMenu.module.scss";
 
+import CONSTANT from "./constants";
+import SearchInput from "components/SearchInput";
+import ResetTag from "./components/ResetTag";
+
 const SideTagsMenu = () => {
+  const [sortType, setSortType] = useState("basic");
   const [tagInputValue, setTagInputValue] = useState("");
 
   const dispatch = useDispatch();
@@ -21,18 +29,55 @@ const SideTagsMenu = () => {
     setTagInputValue(value);
   };
 
+  const sortTagList = () => {
+    if (sortType === "basic") {
+      dispatch(tagListActions.fetchSortTagList({ type: "count" }));
+      setSortType("count");
+    } else {
+      dispatch(tagListActions.fetchSortTagList({ type: "basic" }));
+      setSortType("basic");
+    }
+  };
+
   useEffect(() => {
     dispatch(tagListActions.fetchTagList());
   }, []);
 
   return (
     <div className={styles.SideTagsMenu}>
-      <Input
-        className={styles.SideTagsMenu__input}
-        value={tagInputValue}
-        onChange={handleTagInput}
-      />
-      <Tag tagList={tagList} tagInputValue={tagInputValue} />
+      <div className={styles.SideTagsMenu__container}>
+        <div className={styles.SideTagsMenu__header} />
+        <div className={styles.SideTagsMenu__subHeader}>
+          <span>Tags</span>
+          {sortType === "basic" ? (
+            <SortBigOrderIcon
+              width={CONSTANT.ICON_SIZE.SORT}
+              height={CONSTANT.ICON_SIZE.SORT}
+              color="#3e404c"
+              onClick={sortTagList}
+            />
+          ) : (
+            <SortBasicOrderIcon
+              width={CONSTANT.ICON_SIZE.SORT}
+              height={CONSTANT.ICON_SIZE.SORT}
+              color="#3e404c"
+              onClick={sortTagList}
+            />
+          )}
+        </div>
+        <hr />
+        <div className={cn("d-flex", "align-items-center", "mb-3")}>
+          <SearchInput
+            className={styles.SideTagsMenu__Searchbar}
+            inputClassName={styles.SideTagsMenu__Searchbar__input}
+            value={tagInputValue}
+            placeholder="Search Tags"
+            onChange={handleTagInput}
+          />
+          <ResetTag setTagInputValue={setTagInputValue} />
+        </div>
+        <TagList tagList={tagList} tagInputValue={tagInputValue} />
+      </div>
     </div>
   );
 };
---
+++ client/src/components/SideTagsMenu/constants/index.js
@@ -0,0 +1,7 @@
+const ICON_SIZE = {
+  CLOSE: "20px",
+  REFRESH: "20px",
+  SORT: "24px",
+};
+
+export default { ICON_SIZE };
