9e8d83dd993c2787dfeb7842a705169f255c70d2
pmhxhsj <minho7952@naver.com>
2022-11-12 19:19:50 +0900

    feat: 로그인 페이지 디자인 및 기능 연결

---
+++ client/src/pages/loginPage/LoginPage.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import cn from "classnames";
+import { FullPage, Slide } from "react-full-page";
+
+import Header from "./components/Header";
+import MainTitle from "./components/MainTitle";
+import GoogleOauth from "./components/GoogleOauth";
+
+import Footer from "components/Footer";
+
+import dashboardImage from "common/assets/images/dashboard.png";
+import workspaceImage from "common/assets/images/workspace.png";
+
+import useMoveScroll from "hooks/useMoveScroll";
+
+import styles from "./LoginPage.module.scss";
+
+import CONSTANT from "./constants";
+
+const LoginPage = () => {
+  const { element, onMoveToElement } = useMoveScroll();
+
+  return (
+    <div className={cn(styles.LoginPage)}>
+      <Header onMoveToElement={onMoveToElement} />
+      <section
+        className={cn(
+          styles.LoginPage__container,
+          "d-flex",
+          "justify-content-center",
+          "align-items-center"
+        )}
+      >
+        <div
+          className={cn(
+            styles.LoginPage__Section,
+            "d-flex",
+            "flex-column",
+            "justify-content-between"
+          )}
+        >
+          <FullPage duration={50}>
+            <Slide>
+              <div className={cn(styles.LoginPage__first)}>
+                <div className={cn("d-flex", "align-items-start")}>
+                  <MainTitle
+                    mainText={CONSTANT.FIRST_PAGE_INFO.MAIN}
+                    description={CONSTANT.FIRST_PAGE_INFO.DESCRIPTION}
+                  />
+                  <img
+                    className={styles.LoginPage__image__dashboard}
+                    src={dashboardImage}
+                    alt="dashboard"
+                  />
+                </div>
+              </div>
+            </Slide>
+            <Slide>
+              <div className={cn(styles.LoginPage__second)}>
+                <div className={cn("d-flex", "align-items-start")}>
+                  <MainTitle
+                    mainText={CONSTANT.SECOND_PAGE_INFO.MAIN}
+                    description={CONSTANT.SECOND_PAGE_INFO.DESCRIPTION}
+                  />
+                  <img
+                    className={styles.LoginPage__image__dashboard}
+                    src={workspaceImage}
+                    alt="dashboard"
+                  />
+                </div>
+              </div>
+            </Slide>
+            <Slide>
+              <div className={cn(styles.LoginPage__third)}>
+                <div
+                  className={cn("d-flex", "flex-column", "align-items-center")}
+                >
+                  <div className={styles.LoginPage__third__container}>
+                    <MainTitle
+                      mainText={CONSTANT.THIRD_PAGE_INFO.MAIN}
+                      description={CONSTANT.THIRD_PAGE_INFO.DESCRIPTION}
+                    />
+                    <img
+                      className={styles.LoginPage__image__dashboard}
+                      src={workspaceImage}
+                      alt="dashboard"
+                    />
+                  </div>
+                  <GoogleOauth loginRef={element} />
+                </div>
+              </div>
+            </Slide>
+          </FullPage>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default LoginPage;
--- client/src/pages/loginPage/components/MainTitle/MainTitle.tsx
+++ client/src/pages/loginPage/components/MainTitle/MainTitle.tsx
@@ -1,26 +1,18 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 import styles from "./MainTitle.module.scss";
 
-const TITLE_INFO = {
-  MAIN: <span>always Recordly</span>,
-  DESCRIPTION: (
-    <span>
-      Recordly는 "대학생들을 위한 필기 솔루션"이라는 비전으로 <br />
-      사용자의 메모에 대해 자동 태깅 및 메모 추천을 해줌으로써
-      <br />
-      사용자에게 새로운 경험을 줄 수 있도록 생태계를 만들어 나가고 있습니다.
-    </span>
-  ),
-};
-
-const MainTitle = () => {
+const MainTitle = ({
+  mainText,
+  description,
+}: {
+  mainText: ReactNode;
+  description: ReactNode;
+}) => {
   return (
     <section className={styles.MainTitle}>
-      <div className={styles.MainTitle__main}>{TITLE_INFO.MAIN}</div>
-      <div className={styles.MainTitle__description}>
-        {TITLE_INFO.DESCRIPTION}
-      </div>
+      <div className={styles.MainTitle__main}>{mainText}</div>
+      <div className={styles.MainTitle__description}>{description}</div>
     </section>
   );
 };
