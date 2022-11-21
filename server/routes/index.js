cf5f4426c48811e9bbab06ecd915132ca7d6d322
JadenKim-dev <jaden.kim5179@gmail.com>
2022-11-21 23:09:56 +0900

    feat: session 조회 api 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ import authRouter from "./auth.js";
 import workspaceRouter from "./workspace.js";
 import tagRouter from "./tag.js";
 import folderRouter from "./folder.js";
+import profileRouter from "./profile.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use("/auth", authRouter);
 router.use("/workspace", workspaceRouter);
 router.use("/tag", tagRouter);
 router.use("/folder", folderRouter);
+router.use("/profile", profileRouter);
 
 export default router;
