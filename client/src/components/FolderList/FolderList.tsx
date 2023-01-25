a589e022a79f7a6c6da4a7d520b36f3576ecf5bf
pmhxhsj <minho7952@naver.com>
2023-01-25 23:11:46 +0900

    feat: FolderList react-query로 변환

--- client/src/components/FolderList/FolderList.tsx
+++ client/src/components/FolderList/FolderList.tsx
@@ -1,49 +1,24 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { useSelector } from "react-redux";
 import { useDispatch } from "store";
 import { useNavigate } from "react-router";
 
+import { useGetFolder } from "query-hooks/useFetchFolder";
+
+import { actions as folderActions } from "store/slice/folderSlice";
+
+import WorkspaceSkeleton from "components/Skeleton/WorkspaceSkeleton";
 import Folder from "components/Folder/Folder";
 
 import { IFolder } from "types/folder";
-import { actions as folderActions } from "store/slice/folderSlice";
-import { actions as workspaceActions } from "store/slice/workspaceSlice";
-
-import WorkspaceSkeleton from "components/Skeleton/WorkspaceSkeleton";
-
-// import { IWorkspace } from "types/workspace";
 
 const FolderList = ({ isLoadingData }: { isLoadingData: boolean }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [workspaceList, setWorkspaceList] = useState([]);
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // const workspaceList: IWorkspace[] = useSelector(
-  //   (state: any) => state.workspace.workspaceList
-  // );
-
-  const folderList: IFolder[] = useSelector(
-    (state: any) => state.folder.folderList
-  );
-
-  useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      const response = await axios.get("/api/workspace");
-      const workspaces = response?.data;
-
-      setWorkspaceList(workspaces);
-      setIsLoading(false);
-    })();
-  }, [isLoadingData]);
+  const { data: folders, isLoading } = useGetFolder();
 
   const moveFolderDetailPage = (id: string) => {
     navigate(`/main/${id}`);
     dispatch(folderActions.patchCurrentFolderId({ uid: id }));
-    dispatch(workspaceActions.fetchWorkspaceInFolder({ uid: id }));
   };
 
   return (
@@ -51,12 +26,11 @@ const FolderList = ({ isLoadingData }: { isLoadingData: boolean }) => {
       {isLoadingData ? (
         <WorkspaceSkeleton />
       ) : (
-        folderList.map((folder) => (
+        folders?.map((folder: IFolder) => (
           <Folder
             uid={folder._id}
             key={folder._id}
             title={folder.title}
-            workspaceList={workspaceList}
             isLoading={isLoading}
             moveFolderDetailPage={moveFolderDetailPage}
           />
