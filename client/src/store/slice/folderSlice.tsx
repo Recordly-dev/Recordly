63e74d008a61611d1b871e53c5f2ba5e889d3f5a
pmhxhsj <minho7952@naver.com>
2023-01-26 23:55:34 +0900

    feat: folder slice 최근 id값 관련 전역 상태 빼고 삭제

--- client/src/store/slice/folderSlice.tsx
+++ client/src/store/slice/folderSlice.tsx
@@ -1,84 +1,4 @@
-import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import Swal from "sweetalert2";
-
-const handleError = (err: any) => {
-  if (err.response.data.error === 11000) {
-    Swal.fire({
-      position: "center",
-      icon: "error",
-      title: "Duplicate name found.",
-      showConfirmButton: false,
-      timer: 1000,
-    });
-  } else {
-    Swal.fire({
-      position: "center",
-      icon: "error",
-      title: "Failed to create folder.",
-      showConfirmButton: false,
-      timer: 1000,
-    });
-  }
-};
-/**
- * 모든 폴더 가져오는 로직
- */
-export const fetchFolderList = createAsyncThunk(
-  "folder/fetchFolderList",
-  async () => {
-    const response = await axios.get("/api/folder");
-    return response?.data;
-  }
-);
-
-/**
- * 특정 폴더 삭제하는 로직
- */
-export const deleteFolderList = createAsyncThunk(
-  "folder/deleteFolderList",
-  async (arg: { uid: String }, { dispatch }) => {
-    await axios.delete(`/api/folder/${arg.uid}`);
-
-    dispatch(fetchFolderList());
-  }
-);
-
-/**
- * 폴더 생성하는 로직
- */
-export const postFolderList = createAsyncThunk(
-  "folder/postFolderList",
-  async (arg: { title: String }, { dispatch }) => {
-    try {
-      await axios.post("/api/folder", {
-        title: arg.title,
-      });
-
-      dispatch(fetchFolderList());
-    } catch (err) {
-      handleError(err);
-    }
-  }
-);
-
-/**
- * 폴더 이름 수정하는 로직
- */
-export const patchFolderList = createAsyncThunk(
-  "folder/patchFolderList",
-  async (arg: { uid: String; title: String }, { dispatch }) => {
-    try {
-      await axios.patch(`/api/folder/${arg.uid}`, {
-        folderId: arg.uid,
-        title: arg.title,
-      });
-      dispatch(fetchFolderList());
-    } catch (err) {
-      handleError(err);
-    }
-  }
-);
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 /**
  * 최근 폴더 id값 정의
@@ -95,43 +15,21 @@ export const patchCurrentFolderId = createAsyncThunk(
 );
 
 const initialState = {
-  folderList: [],
   currentFolderId: window.location.pathname.split("/").at(-1),
-  isLoading: false,
 };
 
 const folderSlice = createSlice({
   name: "folder",
   initialState,
-  reducers: {
-    setFolderList: (state, action: PayloadAction<any>) => {
-      state.folderList = action.payload;
-    },
-    setInitialFolderList: (state) => {
-      state.folderList = [];
-    },
-  },
+  reducers: {},
   extraReducers: {
-    [fetchFolderList.pending.type]: (state) => {
-      state.isLoading = true;
-    },
-    [fetchFolderList.fulfilled.type]: (state, action) => {
-      state.folderList = action.payload;
-      state.isLoading = false;
-    },
     [patchCurrentFolderId.fulfilled.type]: (state, action) => {
       state.currentFolderId = action.payload;
     },
   },
 });
 
-export const { setFolderList } = folderSlice.actions;
-
 export const actions = {
-  fetchFolderList,
-  postFolderList,
-  deleteFolderList,
-  patchFolderList,
   patchCurrentFolderId,
   ...folderSlice.actions,
 };
