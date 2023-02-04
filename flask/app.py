74a4e5be1b81727db0924b7d129bcdada5f4c227
JadenKim-dev <jaden.kim5179@gmail.com>
2023-02-04 15:55:23 +0900

    fix: 추천 태그 추출 관련 api 개선

--- flask/app.py
+++ flask/app.py
@@ -9,15 +9,29 @@ def hello():
 
 @application.route('/kobert/tags', methods=['POST'])
 def getTags():
-    text = request.json['text']
-    if not text: return jsonify(tags=[])
+    try:
+        text = request.json['text']
+        if not text: raise Exception("text가 비어있습니다.")
 
-    try: 
         tags = kobert.getTags(text)
-        return jsonify(tags=tags)
+
     except Exception as e:
         print(e)
-        return jsonify(tags=[])
+        data = { 
+            'description': '추천 태그 추출이 불가합니다.',
+            'result': {
+                'tags': []
+            }
+        }
+        return jsonify(data), 200
+
+    data = { 
+        'description': '추천 태그가 정상적으로 조회되었습니다.',
+        'result': {
+            'tags': tags
+        }
+    }
+    return jsonify(data), 200
 
 if __name__ == "__main__":
    application.run(host='0.0.0.0', port=5000, debug=True)
